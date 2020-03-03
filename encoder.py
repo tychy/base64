3dff6415f3fb3bc1f7f2b6317983acd2f1191f2f
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-03-04 08:33:42 +0900

    add : enconder

---
+++ encoder.py
@@ -0,0 +1,60 @@
+def gen_table():
+    table = {}
+    count = 0
+    for i in range(26):
+        table[count] = chr(i + 65)
+        count += 1
+    for i in range(26):
+        table[count] = chr(i + 97)
+        count += 1
+    for i in range(10):
+        table[count] = str(i)
+        count += 1
+    table[count] = "+"
+    table[count+1] = "/"
+    return table
+
+
+TABLE = gen_table()
+# print(TABLE)
+
+
+def hex2binary(h):
+    """
+    convet hex to binary
+    """
+    # print(h)
+    # print(format(int(h, 0), '08b'))
+    return format(int(h, 0), '08b') 
+    
+
+def int2base64(n):
+    assert n < 64 and n >= 0, "n must be 0 to 64"
+    return TABLE[n]
+
+def base64encoder(s):
+    if (s == ""):
+        return ""
+    bit_s = ""
+    for elem in s:
+        ascii_c = hex(ord(elem))
+        #print(ascii_c)
+        bit_s += str(hex2binary(ascii_c))
+        
+    res = ""
+    for i in range(0, len(bit_s), 6):
+        bit_cur = bit_s[i:i+6]
+        if (len(bit_cur) < 6):
+            bit_cur = bit_cur + "0" * (6 - len(bit_cur))
+        ascii_r = int2base64(int(bit_cur, 2))
+        res += ascii_r
+    res = res + "=" * (4 - len(res) % 4)
+    # print(res)
+    return res
+    
+        
+
+
+
+if __name__ == "__main__":
+    base64encoder("ABCDEFG")
